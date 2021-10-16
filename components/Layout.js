4ac217e209baad87bc2243898bf767ef32b5656c
annukkarajala <annukka@rajala.dev>
2021-10-16 21:59:42 +0300

    Clean up main styles and index page

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,15 @@
-import Footer from './Footer';
 import Meta from './Meta';
 import Navbar from './Navbar';
-export const siteTitle = 'Rajala.dev';
+import Footer from './Footer';
+
+export const siteTitle = 'rajala.dev';
 
 export default function Layout({ children }) {
   return (
     <>
       <Meta />
       <Navbar />
-      <article className="content-grid">{children}</article>
+      <div className="content-grid">{children}</div>
       <Footer />
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 
-import AboutMe from '../components/AboutMe';
 import Button from '../components/buttons/Button';
-import Introduction from '../components/Introduction';
 import ProjectCard from '../components/ProjectCard';
 
-import HandWave from '../svgs/handwave.svg';
-import HeroBg from '../svgs/hero-bg.svg';
-import HeroImg from '../svgs/hero-img.svg';
+import SvgHandWave from '../svgs/handwave.svg';
+import SvgDevelopment from '../svgs/development.svg';
+import SvgDevelopmentBg from '../svgs/development-bg.svg';
+import SvgContactLetter from '../svgs/contact-letter.svg';
+import SvgContactLetterBg from '../svgs/contact-letter-bg.svg';
 
 export default function Index() {
   return (
@@ -16,50 +16,69 @@ export default function Index() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <header className="content-grid__full alternate home">
-        <div className="home__bg">
-          <HeroBg />
+      <header className="content-grid__full hero">
+        <div className="hero__bg">
+          <SvgDevelopmentBg />
         </div>
         <div className="content-grid__contained">
-          <div className="home__flex">
+          <div className="hero__flex">
             <div>
-              <Introduction
-                svg={<HandWave />}
-                firstRow="Moikka!"
-                secondRow="Olen Annukka, frontend-kehittäjä"
-              />
-              <div className="home__intro">
-                <h1 className="bigger">
+              <SvgHandWave width="40" height="42" />
+              <p>Moikka!</p>
+              <p>Olen Annukka, frontend-kehittäjä</p>
+              <div className="hero__intro">
+                <h1>
                   Koodia ja designia
                   <br />
                   käsi kädessä
                 </h1>
                 <p>
-                  Web-käyttöliittymien suunnittelu ja toteutus ovat lähellä sydäntä. Vahvinta
-                  osaamis&shy;alaani ovat React, SCSS ja Figma. Opiskelen kolmatta vuotta Jyväskylän
-                  ammatti&shy;korkea&shy;koulussa.
+                  Olen koodari, jolta löytyy myös visuaalista silmää. Etenkin web-käyttöliittymien suunnittelu ja toteutus ovat lähellä sydäntä. Vahvinta osaamistani ovat modernit JavaScript-sovelluskehykset ja CSS.
                 </p>
                 <Button href="" name="Lisää minusta" type="primary" />
                 <Button href="" name="Ota yhteyttä" type="secondary" />
               </div>
             </div>
 
-            <div className="home__illustration">
-              <HeroImg />
+            <div className="hero__illustr">
+              <SvgDevelopment />
             </div>
           </div>
         </div>
       </header>
-      <section className="content-grid__full alternate">
+      <section id="projects" className="content-grid__full">
         <div className="content-grid__contained">
-          <h2 className="bigger">Tuorein työnäyte</h2>
+          <h2>Projektit</h2>
+          <p>Pieniä ja suuria projekteja. Töissä tai vapaa-ajalla, yksin tai osana tiimiä.</p>
           <ProjectCard href="/norma" />
         </div>
       </section>
-      <section id="lisaa-minusta" className="content-grid__full alternate">
+      <section id="about-me" className="content-grid__full">
         <div className="content-grid__contained spacing-bottom">
-          <h2 className="bigger">Lisää minusta</h2>
-          <AboutMe />
+          <h2>Lisää minusta</h2>
+          <p>Elämäntapakoodaaja, joka uppoutuu mielellään myös ruoanlaittoon, liikuntaan tai viherkasvien hoitoon.</p>
+          <div>
+            <p>Vuosi oli ehkä 2009, kun päätin koodata omat netti­sivut. Huomasin pian, että web-kehitys on koukuttava sekoitus luovaa työtä ja ongelman­ratkaisua. Jatkoin netti­sivujen rakentelua, ja koodaaminen alkoi vetää puoleensa eri tavalla kuin muut harrastukset. Urapolkua valitessa web-kehitys tuntui itsestäänselvältä.</p>
+            <p>Alla muutamia teknologioita ja työkaluja, joiden parissa olen työskennellyt viime aikoina.</p>
+            <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="contact" className="content-grid__full">
+        <div className="content-grid__contained spacing-bottom">
+          {/* <SvgContactLetter /> */}
+          {/* <SvgContactLetterBg /> */}
+          <h2>Ota yhteyttä</h2>
+          <p>Jos haluat jutella, laita postia osoitteeseen <a href="mailto:annukka@rajala.dev">annukka@rajala.dev</a> tai ota yhteyttä <a
+            href="https://www.linkedin.com/in/annukkarajala/"
+            target="_blank"
+            rel="noopener noreferrer">
+            LinkedIn
+          </a>issä.</p>
         </div>
       </section>
     </Layout>
