66ca9f1dd1dc2515aa78f16023eea7e7ebee455f
annukkarajala <annukka@rajala.dev>
2021-11-09 20:58:10 +0200

    Set default theme to dark

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '@/styles/main.scss'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider>
+    <ThemeProvider defaultTheme="dark">
       <Component {...pageProps} />
     </ThemeProvider>
   )
