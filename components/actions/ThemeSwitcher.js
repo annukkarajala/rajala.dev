eb95cea7421136e54cae6fe1bb4ead0aaab4e87f
annukkarajala <annukka@rajala.dev>
2021-11-10 20:46:27 +0200

    Improve theme

--- components/actions/ThemeSwitcher.js
+++ components/actions/ThemeSwitcher.js
@@ -1,18 +1,32 @@
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
+import svgDark from '@/svgs/icons/theme-dark.svg'
+import svgLight from '@/svgs/icons/theme-light.svg'
+
+const iconTypes = {
+  light: svgLight,
+  dark: svgDark,
+}
 
 export default function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const { resolvedTheme, setTheme } = useTheme()
 
   // get access to theme after mounting
   useEffect(() => setMounted(true), [])
 
+  const inactiveTheme = resolvedTheme === 'light' ? 'dark' : 'light'
+  const Icon = iconTypes[inactiveTheme]
+
   return (
-    <div>
+    <div className="theme-switcher__wrapper">
       {mounted && (
-        <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-          {theme}
+        <button
+          className="theme-switcher"
+          onClick={() =>
+            setTheme(resolvedTheme === 'light' ? 'dark' : 'light')
+          }>
+          <Icon width="24" height="24" />
         </button>
       )}
     </div>
